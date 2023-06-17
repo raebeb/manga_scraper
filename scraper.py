166aef98ed483adf2e806caffb7750ea8984034c
Javier pacheco <javier.pacheco@niufoods.cl>
2023-06-16 23:40:47 -0400

    Title: Refactor command line argument parsing and update documentation
    Details:
    
    - Replaced 'getopt' library with 'argparse' for handling command line arguments. This provides more flexibility and makes the code easier to maintain and understand.
    
    - Updated documentation to reflect changes in command line arguments and provided additional clarity on their usage.
    
    - Enhanced error handling for missing arguments and provided clearer error messages for the user.

--- scraper.py
+++ scraper.py
@@ -1,46 +1,23 @@
-import sys
-from getopt import getopt
-
+import argparse
 from utils import download_manga
 
-
 if __name__ == "__main__":
-    myopts, _ = getopt(sys.argv[1:], "n:i:f:l:s:")
-    if not myopts:
-        raise KeyError("There's no arguments!")
+    parser = argparse.ArgumentParser(description='Download manga from Manga Tigre.')
 
-    manga_title = None
-    params = {}
+    parser.add_argument('manga_title', type=str, help='Title of the manga')
+    parser.add_argument('-i', '--initial_chapter', type=int, help='Initial chapter to download')
+    parser.add_argument('-f', '--final_chapter', type=str, help='Final chapter to download')
+    parser.add_argument('-l', '--download_last_chapter', action='store_true', help='Download the last chapter')
+    parser.add_argument('-s', '--single_chapter', type=int, help='Download a single chapter')
 
-    params_dict = {
-        "-n": {
-            "variable": "manga_title",
-            "type": str,
-        },
-        "-i": {
-            "variable": "initial_chapter",
-            "type": int,
-        },
-        "-f": {
-            "variable": "final_chapter",
-            "type": str,
-        },
-        "-l": {
-            "variable": "download_last_chapter",
-            "type": bool,
-        },
-        "-s": {
-            "variable": "single_chapter",
-            "type": int,
-        },
-    }
+    args = parser.parse_args()
 
-    for key, value in myopts:
-        arg = params_dict.get(key)
-        params[arg["variable"]] = arg["type"](value)
+    if not any(vars(args).values()):
+        parser.error("No arguments provided!")
 
-    download_manga(**params)
+    else:
+        params = vars(args)
+        download_manga(**params)
 
     print("Done!")
     print("Bye! ฅ^•ﻌ•^ฅ ")
-
